bc7da41ef194e00cab5186e9bd79e4e0b2380d7e
Israt Jahan <isratbiswas28@gmail.com>
2025-03-09 20:20:52 +0600

    forth commit

--- models/products.js
+++ models/products.js
@@ -4,9 +4,9 @@ const productSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   category: { type: String, required: true },
-  // stock: { type: String, required: true },
-  // description: { type: String },
-  // createdAt: { type: Date, default: Date.now },
+  stock: { type: String, required: true },
+  description: { type: String },
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports.Product = model("Product", productSchema);
